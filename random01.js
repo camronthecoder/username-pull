35dba35f6ac63410e958ac8a15dd4d3875701e70
camronthecoder <106614872+camronthecoder@users.noreply.github.com>
2022-06-02 12:04:50 -0400

    first draft

---
+++ random01.js
@@ -0,0 +1,23 @@
+let url = `https://api.github.com/users`
+const xhr = new XMLHttpRequest()
+xhr.open('GET', url)
+xhr.send()
+xhr.onload=()=>{
+
+    const jsonData = JSON.parse(xhr.responseText)
+    console.log("parsed", jsonData)
+    jsonArray=[]
+    jsonArray.push(jsonData[0].node_id)//correrct 
+
+    for (let i = 0; i < jsonData.length; i++) {
+        jsonArray.push(jsonData[i].login) //wrong answer
+        
+    }
+    jsonArray.splice(5)
+    randomArray=[]
+
+    while(jsonArray.length>0){
+        randomArray.push
+
+    }
+console.log(jsonArray)}
